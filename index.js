d72c1ec4720d58186b8faa4078af100254c3d2f9
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-01 12:45:57 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const result = array.find( function(obj) { return obj.result === "W" })
+  return result ? result.year : undefined
+}
